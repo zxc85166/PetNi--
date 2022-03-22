c0173d06468ad995dd37a34d310b102551213591
iiiteam11801 <iiiteam11801@gmail.com>
2022-03-22 19:36:48 +0800

    配對分頁

--- src/components/ButtonRepo.vue
+++ src/components/ButtonRepo.vue
@@ -1,10 +1,8 @@
+<script setup>
+defineProps({
+  msg: String
+})
+</script>
 <template>
-  <div class="ml-3 inline-flex rounded-md shadow">
-    <a
-      target="_blank"
-      href="https://github.com/web2033/vite-vue3-tailwind-starter"
-      class="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium leading-6 text-indigo-600 transition duration-150 ease-in-out hover:text-indigo-500 focus:outline-none"
-      >Learn more</a
-    >
-  </div>
+  <button class="btn rounded-3xl">{{ msg }}</button>
 </template>
--- src/views/Match.vue
+++ src/views/Match.vue
@@ -1,5 +1,6 @@
 <script setup>
 import navbar from "@/components/navbar.vue";
+import ButtonRepoVue from "@/components/ButtonRepo.vue";
 </script>
 
 <template>
@@ -7,9 +8,63 @@ import navbar from "@/components/navbar.vue";
     <navbar />
   </header>
   <div class="grid grid-cols-3 gap-0">
-    <div class="bg-gray-100 box-shadow px-5 py-5">
-      <div class="h-full">
-        <span>我想尋找</span>
+    <div class="bg-gray-100 box-shadow pl-20 py-5">
+      <div>
+        <p class="font-black">我想尋找</p>
+        <button class="btn rounded-3xl w-[90px] h-[90px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
+      <div>
+        <p class="font-black">性別</p>
+        <button class="btn rounded-3xl w-[90px] h-[90px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
+      <div>
+        <p class="font-black">年齡</p>
+        <button class="btn rounded-[14px] w-[90px] h-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </div>
     </div>
     <div class="col-span-2">09</div>
