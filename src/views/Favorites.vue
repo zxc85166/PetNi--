6e12aada589ebc7bcbe9a6d7e9dd7a2b8518f37f
罐頭 <jimmya011@gmail.com>
2022-03-30 22:08:03 +0800

    測試：web API => navigator.share

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,9 +1,22 @@
-<template >
-    <div></div>
-</template>
 <script setup>
-
+const share = () => {
+    if (navigator.share) {
+        navigator.share({
+            title: '分享標題',
+            text: '分享文字',
+            url: 'https://www.twitch.tv/japan_asmr'
+        })
+    } else {
+        navigator.clipboard.writeText('Code is here')
+    }
+}
 </script>
+<template >
+    <div>
+        <button @click="share" class="btn-lg">點我</button>
+    </div>
+</template>
+
 <style lang="">
     
 </style>
